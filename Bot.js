bb63490d974dcee65f4c5edfc997a5287450fd37
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-06-23 01:16:47 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -2,9 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    
+    // Set bot status to: "Playing with JavaScript"
     client.user.setActivity("Over you (Sorry b0t anavable) ",  {type: "WATCHING"})
-    
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    // client.user.setActivity("OVER YOU", {type: "WATCHING"})
 });
 
 client.on('message', message => {
@@ -26,4 +30,4 @@ client.on('message', message => {
   }
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
