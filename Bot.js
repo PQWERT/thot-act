a553a76fba1f0499154e0513b6fe60cd7002f228
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-06-23 00:22:14 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
 
 client.on('ready', () => {
     client.user.setActivity("Over you (Sorry b0t anavable) ",  {type: "WATCHING"})
