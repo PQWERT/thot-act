8680c28a2e2f4c3e983c5aac01c349cac925f40a
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-06-23 02:09:19 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -11,23 +11,4 @@ client.on('ready', () => {
     // client.user.setActivity("OVER YOU", {type: "WATCHING"})
 });
 
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
-
-  if (message.content === 'p!join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('Done!');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
-
 client.login(process.env.BOT_TOKEN);
