15c85a58425630949723952965f60af637009c2e
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-07-03 01:31:43 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
 labelname:
+for client.on('ready', () => {
     client.user.setActivity("p!music",  {type: "PLAYING"});
     setTimeout(function(){ 
     client.user.setActivity("Sorry Music only for now",  {type: "LISTENING"} , 15000);
