4c899c7c2cd902d2c3278cf9000d31ef21a426bf
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-07-03 01:16:02 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -6,6 +6,8 @@ client.on('ready', () => {
     client.user.setActivity("p!music",  {type: "PLAYING"});
    setTimeout(function(){ 
     client.user.setActivity("Sorry Music only for now",  {type: "LISTENING"} , 15000);
+    }
 continue loop;
-
+} 
+});
 client.login(process.env.BOT_TOKEN);
