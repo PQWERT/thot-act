6a68866768f9006c3743f1dcffecd454cc4386fc
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-07-02 17:08:36 -0400

    Add files via upload

---
+++ Bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var a = 0;
+beginning: while(true) {
+client.on('ready', () => {
+    // Set bot status to: "Playing with JavaScript"
+    client.user.setActivity("p!music",  {type: "PLAYING"});
+   
+setTimeout(function(){ 
+    client.user.setActivity("Sorry Music only for now",  {type: "LISTENING"} , 15000);
+
+a++;
+   if(i < 424) continue beginning;
+      break;
+}
+
+client.login(process.env.BOT_TOKEN);
