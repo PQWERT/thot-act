24346a5dc35b0b7a15153e1dc05e5cd3655853b6
PQWERT <50163952+PQWERT@users.noreply.github.com>
2019-07-03 01:17:37 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-Loop:
+Loop1:
 client.on('ready', () => {
     client.user.setActivity("p!music",  {type: "PLAYING"});
    setTimeout(function(){ 
     client.user.setActivity("Sorry Music only for now",  {type: "LISTENING"} , 15000);
     });
-continue loop;
+continue loop1;
 } 
 });
 client.login(process.env.BOT_TOKEN);
